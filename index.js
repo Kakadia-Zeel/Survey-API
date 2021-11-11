dd2e6f39642c87cbf9c41d3bda5c8b1f2d7e890f
Kakadia-Zeel <zkzeelkakadiya@gmail.com>
2021-11-12 02:28:23 +0530

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express()
 require("dotenv").config();
 
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 
 // Middleware
 // for converting the json part of the request body
